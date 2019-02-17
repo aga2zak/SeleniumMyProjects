fee3fdebcff58c6d477dcee3255645cb20349295
aga2zak <aga2.zak@gmail.com>
2019-02-17 14:01:52 +0100

    my works

--- seleniumBasic/src/main/java/pl/infoshare/utils/SeleniumApp.java
+++ seleniumBasic/src/main/java/pl/infoshare/utils/SeleniumApp.java
@@ -15,6 +15,7 @@ public class SeleniumApp {
         String url = "http://demo.shopizer.com:8080/shop/";
         WebDriver driver = new ChromeDriver();
         driver.get(url);
+        driver.manage().window().maximize();
 
         WebElement handbagMenu = driver.findElement(By.xpath(selector.getHandbagsLink()));
         handbagMenu.click();
@@ -39,8 +40,74 @@ public class SeleniumApp {
         carActionSequence.moveToElement(cart).click().build().perform();
 
         WebElement chackoutLink =
-                wait.until((ExpectedConditions.elementToBeClickable((By.xpath(selector.getChackoutLink())))));
+                wait.until((ExpectedConditions.elementToBeClickable((By.cssSelector(selector.getChackoutLink())))));
         chackoutLink.click();
+
+
+        WebElement proceedTpCheckoutLink =
+                wait.until((ExpectedConditions.elementToBeClickable((By.cssSelector(selector.getProceedTpCheckoutLink())))));
+
+        proceedTpCheckoutLink.click();
+
+
+        WebElement ferstNameInput =
+                wait.until((ExpectedConditions.elementToBeClickable((By.cssSelector(selector.getFerstNameInput())))));
+
+        ferstNameInput.sendKeys("Jan");
+
+        WebElement lastNameInput =
+                wait.until((ExpectedConditions.elementToBeClickable((By.cssSelector(selector.getLastNameInput())))));
+
+        lastNameInput.sendKeys("Nowak");
+
+        WebElement streetInput =
+                wait.until((ExpectedConditions.elementToBeClickable((By.cssSelector(selector.getStreetInput())))));
+
+        streetInput.sendKeys("Polna 10/7");
+
+        WebElement cityInput =
+                wait.until((ExpectedConditions.elementToBeClickable((By.cssSelector(selector.getCityInput())))));
+
+        cityInput.sendKeys("Gdynia");
+
+        WebElement countrySelect =
+                wait.until((ExpectedConditions.elementToBeClickable((By.cssSelector(selector.getCountrySelect())))));
+
+        countrySelect.sendKeys("Brasil");
+
+        WebElement stateInput =
+                wait.until((ExpectedConditions.elementToBeClickable((By.cssSelector(selector.getStateInput())))));
+
+        stateInput.click();
+        stateInput.sendKeys("Gdynia");
+
+        WebElement postalCodeInput =
+                wait.until((ExpectedConditions.elementToBeClickable((By.cssSelector(selector.getPostalCodeInput())))));
+
+        postalCodeInput.sendKeys("11-111");
+
+        WebElement emailInput =
+                wait.until((ExpectedConditions.elementToBeClickable((By.cssSelector(selector.getEmailInput())))));
+
+        emailInput.sendKeys("test@test.com");
+
+        WebElement phoneNumberInput =
+                wait.until((ExpectedConditions.elementToBeClickable((By.cssSelector(selector.getPhoneNumberInput())))));
+
+        phoneNumberInput.sendKeys("+48 111222333");
+
+
+        WebElement cart1 =
+                wait.until((ExpectedConditions.elementToBeClickable((By.cssSelector(selector.getSubmitOrderButton())))));
+
+        //submitOrderButton.click();
+
+        Actions submitOrderButton = new Actions(driver);
+        submitOrderButton.moveToElement(cart1).click().build().perform();
+
+
+
+        driver.close();
 
 //
 //        WebElement checkout = wait.until(ExpectedConditions.elementToBeClickable
