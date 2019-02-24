9ae9f520a6efccdc12d1930dafb28afbf94342d0
aga2zak <aga2.zak@gmail.com>
2019-02-24 14:10:39 +0100

    my works

---
+++ seleniumBasic/src/main/java/pl/infoshare/utils/PurchaseSelectorsBy.java
@@ -0,0 +1,81 @@
+package pl.infoshare.utils;
+
+import org.openqa.selenium.By;
+
+public class PurchaseSelectorsBy {
+    private By HandbagsLink=By.xpath("//div[contains(@class, 'mainmenu')]//a[text()='Handbags']");
+    private By ChickVintageDeVilleLink=By.cssSelector("#productsContainer > .col-md-4:nth-child(2) .addToCart");
+    private By ShippingCartLink=By.xpath("//*[@id='miniCartSummary']/a[text()='Shopping cart ']");
+    private By ChackoutLink=By.cssSelector(".checkout-bg > a");
+    private By ProceedTpCheckoutLink=By.cssSelector(".wc-proceed-to-checkout > a");
+    private By FerstNameInput=By.cssSelector("input[name='customer.billing.firstName']");
+    private By LastNameInput=By.cssSelector("input[name='customer.billing.lastName']");
+    private By StreetInput=By.cssSelector("input[name='customer.billing.address']");
+    private By CityInput=By.cssSelector("input[name='customer.billing.city']");
+    private By CountrySelect=By.cssSelector("select[name='customer.billing.country']");
+    private By StateInput=By.cssSelector("input[name='customer.billing.stateProvince']");
+    private By PostalCodeInput=By.cssSelector("input[name='customer.billing.postalCode']");
+    private By EmailInput=By.cssSelector("input[name='customer.emailAddress']");
+    private By PhoneNumberInput=By.cssSelector("input[name='customer.billing.phone']");
+    private By SubmitOrderButton=By.id("submitOrder");
+
+    public By getHandbagsLink() {
+        return HandbagsLink;
+    }
+
+    public By getChickVintageDeVilleLink() {
+        return ChickVintageDeVilleLink;
+    }
+
+    public By getShippingCartLink() {
+        return ShippingCartLink;
+    }
+
+    public By getChackoutLink() {
+        return ChackoutLink;
+    }
+
+    public By getProceedTpCheckoutLink() {
+        return ProceedTpCheckoutLink;
+    }
+
+    public By getFerstNameInput() {
+        return FerstNameInput;
+    }
+
+    public By getLastNameInput() {
+        return LastNameInput;
+    }
+
+    public By getStreetInput() {
+        return StreetInput;
+    }
+
+    public By getCityInput() {
+        return CityInput;
+    }
+
+    public By getCountrySelect() {
+        return CountrySelect;
+    }
+
+    public By getStateInput() {
+        return StateInput;
+    }
+
+    public By getPostalCodeInput() {
+        return PostalCodeInput;
+    }
+
+    public By getEmailInput() {
+        return EmailInput;
+    }
+
+    public By getPhoneNumberInput() {
+        return PhoneNumberInput;
+    }
+
+    public By getSubmitOrderButton() {
+        return SubmitOrderButton;
+    }
+}
---
+++ seleniumBasic/src/main/java/pl/infoshare/utils/SeleniumTest.java
@@ -0,0 +1,141 @@
+package pl.infoshare.utils;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class SeleniumTest {
+
+    private WebDriver driver;
+    private PurchaseSelectors selector;
+    private String url;
+
+
+    @Before
+     public void startBrowser() {
+         selector = new PurchaseSelectors();
+         url = "http://demo.shopizer.com:8080/shop/";
+         driver = new ChromeDriver();
+        WebDriverWait wait = new WebDriverWait(driver, 20);
+        JavascriptExecutor runner = (JavascriptExecutor) driver;
+//        runner.executeScript("return document.readyState");
+//        runner.executeScript("return document.readyState").equals("complete");
+
+        wait.until((ExpectedCondition<Boolean>) driver ->
+            (runner.executeScript("return document.readyState").equals("complete")));
+
+    }
+
+    @After
+    public void closeBrowser() {
+        driver.quit();
+    }
+
+    @Test
+    public void purchase() {
+        driver.get(url);
+        driver.manage().window().maximize();
+
+        WebElement handbagMenu = driver.findElement(By.xpath(selector.getHandbagsLink()));
+        handbagMenu.click();
+
+        WebDriverWait wait = new WebDriverWait(driver, 10);
+
+
+        String select = selector.getChickVintageDeVilleLink();
+        //By cssSelector = By.cssSelector(select);
+        //ExpectedConditions.elementToBeClickable((cssSelector));
+
+
+        WebElement chickVintageDeVilleLink = driver.findElement(By.cssSelector(selector.getChickVintageDeVilleLink()));
+        chickVintageDeVilleLink.click();
+
+
+        WebElement cart =
+                wait.until((ExpectedConditions.elementToBeClickable((By.xpath(selector.getShippingCartLink())))));
+        cart.click();
+
+
+        Actions carActionSequence = new Actions(driver);
+        carActionSequence.moveToElement(cart).click().build().perform();
+
+        WebElement chackoutLink =
+                wait.until((ExpectedConditions.elementToBeClickable((By.cssSelector(selector.getChackoutLink())))));
+        chackoutLink.click();
+
+
+        WebElement proceedTpCheckoutLink =
+                wait.until((ExpectedConditions.elementToBeClickable((By.cssSelector(selector.getProceedTpCheckoutLink())))));
+
+        JavascriptExecutor runner = (JavascriptExecutor) driver;
+        String script = "arguments[0].click();";
+        runner.executeScript(script, proceedTpCheckoutLink);
+        //proceedTpCheckoutLink.click();
+
+
+        WebElement ferstNameInput =
+                wait.until((ExpectedConditions.elementToBeClickable((By.cssSelector(selector.getFerstNameInput())))));
+
+        ferstNameInput.sendKeys("Jan");
+
+        WebElement lastNameInput =
+                wait.until((ExpectedConditions.elementToBeClickable((By.cssSelector(selector.getLastNameInput())))));
+
+        lastNameInput.sendKeys("Nowak");
+
+        WebElement streetInput =
+                wait.until((ExpectedConditions.elementToBeClickable((By.cssSelector(selector.getStreetInput())))));
+
+        streetInput.sendKeys("Polna 10/7");
+
+        WebElement cityInput =
+                wait.until((ExpectedConditions.elementToBeClickable((By.cssSelector(selector.getCityInput())))));
+
+        cityInput.sendKeys("Gdynia");
+
+        WebElement countrySelect =
+                wait.until((ExpectedConditions.elementToBeClickable((By.cssSelector(selector.getCountrySelect())))));
+
+        countrySelect.sendKeys("Brasil");
+
+        WebElement stateInput =
+                wait.until((ExpectedConditions.elementToBeClickable((By.cssSelector(selector.getStateInput())))));
+
+        stateInput.click();
+        stateInput.sendKeys("Gdynia");
+
+        WebElement postalCodeInput =
+                wait.until((ExpectedConditions.elementToBeClickable((By.cssSelector(selector.getPostalCodeInput())))));
+
+        postalCodeInput.sendKeys("11-111");
+
+        WebElement emailInput =
+                wait.until((ExpectedConditions.elementToBeClickable((By.cssSelector(selector.getEmailInput())))));
+
+        emailInput.sendKeys("test@test.com");
+
+        WebElement phoneNumberInput =
+                wait.until((ExpectedConditions.elementToBeClickable((By.cssSelector(selector.getPhoneNumberInput())))));
+
+        phoneNumberInput.sendKeys("+48 111222333");
+
+
+        WebElement cart1 =
+                wait.until((ExpectedConditions.elementToBeClickable((By.cssSelector(selector.getSubmitOrderButton())))));
+
+        //submitOrderButton.click();
+
+        Actions submitOrderButton = new Actions(driver);
+        submitOrderButton.moveToElement(cart1).click().build().perform();
+
+    }
+}
